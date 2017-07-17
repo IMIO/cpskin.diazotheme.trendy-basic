b7508eef11c1379bcadf56211b955e94e11bb0a1
Benoît Suttor <bsuttor@imio.be>
2017-07-17 10:59:37 +0200

    Back to development: 0.2.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Installer for the cpskin.diazotheme.trendybasic package."""
 
-version = '0.2.2'
+version = '0.2.3.dev0'
 
 from setuptools import find_packages
 from setuptools import setup
